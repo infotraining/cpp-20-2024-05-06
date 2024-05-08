8cea23f4f7c1e8c46c9b3fdde5b3d3245a16f1c1
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-05-08 10:25:42 +0200

    Ranges - done

--- _exercises/ex-ranges/ranges.cpp
+++ _exercises/ex-ranges/ranges.cpp
@@ -51,7 +51,7 @@ TEST_CASE("split")
 TEST_CASE("Exercise - ranges")
 {
     const std::vector<std::string_view> lines = { 
-        "# Comment 1", /* comments can be only at the begining of a file */
+        "# Comment 1", /* comments can be only at the beginning of a file */
         "# Comment 2",
         "# Comment 3",
         "1/one",
@@ -65,13 +65,247 @@ TEST_CASE("Exercise - ranges")
         "6/six"
     };
 
+    auto result = lines
+        | std::views::drop_while([]( std::string_view x ) { return x.starts_with('#'); } )
+        | std::views::filter([]( std::string_view x ) { return x != "\n"; })
+        | std::views::transform([](std::string_view x) { return split(x); })
+        | std::views::values;
+        //| std::views::common; // harmonizing types returned from begin() & end()
+        //| std::ranges::to<std::vector>(); // C+++23
+        
     helpers::print(lines, "lines");
-
-    auto result = lines;
 
     helpers::print(result, "result");
 
     auto expected_result = {"one"s, "two"s, "three"s, "four"s, "five"s, "six"s};
 
-    //CHECK(std::ranges::equal(result, expected_result));
+    CHECK(std::ranges::equal(result, expected_result));
+
+    auto common_wrapper = std::views::common(result);
+    std::vector<std::string_view> vec_result(common_wrapper.begin(), common_wrapper.end());
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+namespace rng = std::ranges;
+
+template <rng::input_range TRange>
+    requires rng::view<TRange>
+class EachNthView : public rng::view_interface<EachNthView<TRange>>
+{
+    using IterDifference = rng::range_difference_t<TRange>;
+
+    TRange range_{};
+    IterDifference n_{};
+    rng::iterator_t<TRange> iter_{};
+    rng::sentinel_t<TRange> end_{};
+
+public:
+    struct EachNthIterator
+    {
+        using iterator_tag = std::input_iterator_tag;
+        using value_type = std::ranges::range_value_t<TRange>;
+        using difference_type = IterDifference;
+        using reference = std::ranges::range_reference_t<TRange>;
+        using pointer = std::remove_reference_t<std::ranges::range_reference_t<TRange>>*;
+
+        rng::iterator_t<TRange> iter_{};
+        rng::sentinel_t<TRange> end_{};
+        IterDifference n_{};
+
+        EachNthIterator& operator++()
+        {
+            for (IterDifference i = 0; i < n_; ++i)
+            {
+                ++iter_;
+                if (iter_ == end_)
+                    break;
+            }
+            return *this;
+        }
+
+        EachNthIterator operator++(int)
+        {
+            auto tmp = *this;
+            ++*this;
+            return tmp;
+        }
+
+        reference operator*() const
+        {
+            return *iter_;
+        }
+
+        bool operator==(const EachNthIterator& other) const
+        {
+            return iter_ == other.iter_;
+        }
+    };
+
+    EachNthView() = default;
+
+    EachNthView(TRange range, size_t n)
+        : range_{std::move(range)}
+        , n_(n)
+        , iter_{rng::begin(range_)}
+        , end_{rng::end(range_)}
+    {
+    }
+
+    auto begin()
+    {
+        return EachNthIterator{range_.begin(), end_, n_};
+    }
+
+    auto end()
+    {
+        return EachNthIterator{range_.end(), end_, n_};
+    }
+};
+
+// deduction guide
+template <class TRange>
+EachNthView(TRange&& base, size_t n) -> EachNthView<rng::views::all_t<TRange>>;
+
+// pipe operator
+namespace Details
+{
+    struct EachNthRangeAdaptorClosure
+    {
+        std::size_t n_;
+        constexpr EachNthRangeAdaptorClosure(std::size_t n)
+            : n_(n)
+        { }
+
+        template <rng::viewable_range R>
+        constexpr auto operator()(R&& r) const
+        {
+            return EachNthView(std::forward<R>(r), n_);
+        }
+    };
+
+    struct EachNthRangeAdaptor
+    {
+        template <rng::viewable_range R>
+        constexpr auto operator()(R&& r, rng::range_difference_t<R> n)
+        {
+            return EachNthView(std::forward<R>(r), n);
+        }
+
+        constexpr auto operator()(std::size_t n)
+        {
+            return EachNthRangeAdaptorClosure(n);
+        }
+    };
+
+    template <rng::viewable_range R>
+    constexpr auto operator|(R&& r, const EachNthRangeAdaptorClosure& each_nth_adaptor)
+    {
+        return each_nth_adaptor(std::forward<R>(r));
+    }
+} // namespace Details
+
+namespace views
+{
+    Details::EachNthRangeAdaptor each_nth;
+}
+
+TEST_CASE("skipping view")
+{
+    std::vector vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto all_vec = std::views::all(vec);
+    std::vector<int> results;
+
+    SECTION("nth = 1")
+    {
+        auto view = EachNthView{all_vec, 1};
+
+        static_assert(std::weakly_incrementable<decltype(view.begin())>);
+        static_assert(std::input_or_output_iterator<decltype(view.begin())>);
+        static_assert(std::input_iterator<decltype(view.begin())>);
+        static_assert(std::ranges::input_range<decltype(view)>);
+        static_assert(std::ranges::view<decltype(view)>);
+
+        std::ranges::copy(view, std::back_inserter(results));
+        CHECK(results == vec);
+    }
+
+    SECTION("nth = 2")
+    {
+        auto view = EachNthView{vec, 2};
+
+        std::ranges::copy(view, std::back_inserter(results));
+        CHECK(results == std::vector{1, 3, 5, 7, 9});
+    }
+
+    SECTION("nth = 3")
+    {
+        auto view = EachNthView{vec, 3};
+
+        std::ranges::copy(view, std::back_inserter(results));
+        CHECK(results == std::vector{1, 4, 7, 10});
+    }
+
+    SECTION("nth = 4")
+    {
+        auto view = EachNthView{all_vec, 4};
+
+        std::ranges::copy(view, std::back_inserter(results));
+        CHECK(results == std::vector{1, 5, 9});
+    }
+
+    SECTION("nth = 5")
+    {
+        auto view = EachNthView{all_vec, 5};
+
+        std::ranges::copy(view, std::back_inserter(results));
+        CHECK(results == std::vector{1, 6});
+    }
+
+    SECTION("nth = 9")
+    {
+        auto view = EachNthView{all_vec, 9};
+
+        std::ranges::copy(view, std::back_inserter(results));
+        CHECK(results == std::vector{1, 10});
+    }
+
+    SECTION("nth = 10")
+    {
+        auto view = EachNthView{all_vec, 10};
+
+        std::ranges::copy(view, std::back_inserter(results));
+        CHECK(results == std::vector{1});
+    }
+
+    SECTION("overriting items")
+    {
+        auto view = EachNthView{all_vec, 2};
+
+        for (auto& item : view)
+            item *= 10;
+
+        CHECK(vec == std::vector{10, 2, 30, 4, 50, 6, 70, 8, 90, 10});
+    }
+
+    SECTION("adaptor")
+    {
+        SECTION("applied once")
+        {
+            auto view = vec | views::each_nth(2);
+
+            std::ranges::copy(view, std::back_inserter(results));
+            CHECK(results == std::vector{1, 3, 5, 7, 9});
+        }
+
+        SECTION("applied twice")
+        {
+            auto view = vec 
+                            | views::each_nth(2)
+                            | views::each_nth(2);
+
+            std::ranges::copy(view, std::back_inserter(results));
+            CHECK(results == std::vector{1, 5, 9});
+        }
+    }
 }
--- ranges/ranges_and_views.cpp
+++ ranges/ranges_and_views.cpp
@@ -1,10 +1,12 @@
+#include <algorithm>
 #include <catch2/catch_test_macros.hpp>
+#include <format>
 #include <helpers.hpp>
 #include <iostream>
+#include <map>
 #include <ranges>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std::literals;
 
@@ -13,32 +15,34 @@ TEST_CASE("ranges", "[ranges]")
     auto data = helpers::create_numeric_dataset<20>(42);
     helpers::print(data, "data");
 
-    std::vector words = {"one"s, "two"s, "three"s, "four"s, "five"s, "six"s, "seven"s, "eight"s, "nine"s, "ten"s, 
-                         "eleven"s, "twelve"s, "thirteen"s, "fourteen"s, "fifteen"s, "sixteen"s, "seventeen"s, "eighteen"s, "nineteen"s, "twenty"s};
+    std::vector words = {"one"s, "two"s, "three"s, "four"s, "five"s, "six"s, "seven"s, "eight"s, "nine"s, "ten"s,
+        "eleven"s, "twelve"s, "thirteen"s, "fourteen"s, "fifteen"s, "sixteen"s, "seventeen"s, "eighteen"s, "nineteen"s, "twenty"s};
     helpers::print(words, "words");
 
     SECTION("algorithms")
     {
-        //std::sort(data.begin(), data.end());
+        // std::sort(data.begin(), data.end());
         std::ranges::sort(data, std::greater{});
         CHECK(std::ranges::is_sorted(data, std::greater{}));
         helpers::print(data, "data");
 
         std::vector<int> negative_numbers;
-        std::ranges::copy_if(data, std::back_inserter(negative_numbers), [](int n) { return n < 0; });
+        std::ranges::copy_if(data, std::back_inserter(negative_numbers), [](int n)
+            { return n < 0; });
         helpers::print(negative_numbers, "negative_numbers");
     }
 
     SECTION("projections")
     {
-        std::vector<std::string> words = { "c++", "c", "rust", "php", "kotlin" };
+        std::vector<std::string> words = {"c++", "c", "rust", "php", "kotlin"};
 
-        //std::sort(words.begin(), words.end(), [](const auto& a, const auto& b) { return a.size() > b.size(); });
-        std::ranges::sort(words, [](const auto& a, const auto& b) { return a.size() > b.size(); });
+        // std::sort(words.begin(), words.end(), [](const auto& a, const auto& b) { return a.size() > b.size(); });
+        std::ranges::sort(words, [](const auto& a, const auto& b)
+            { return a.size() > b.size(); });
 
-        std::ranges::sort(words, std::greater{}, /*projection*/ std::ranges::size );  
+        std::ranges::sort(words, std::greater{}, /*projection*/ std::ranges::size);
 
-        helpers::print(words, "words");      
+        helpers::print(words, "words");
     }
 
     SECTION("concepts & tools")
@@ -94,33 +98,149 @@ TEST_CASE("views")
 
     SECTION("all")
     {
+        auto all_items = std::views::all(data);
+
+        helpers::print(all_items, "all_items");
     }
 
     SECTION("subrange - iterator & sentinel as a view")
     {
+        auto head = std::ranges::subrange(data.begin(), EndValue<42>{});
+
+        helpers::print(head, "head");
+
+        std::ranges::fill(head, 0);
+
+        helpers::print(head, "head");
     }
 
     SECTION("counted")
-    {        
+    {
+        auto first_3 = std::views::counted(data.begin(), 3);
+
+        std::ranges::fill(first_3, -1);
+
+        helpers::print(first_3, "first_3");
     }
 
     SECTION("iota")
     {
+        helpers::print(std::views::iota(1, 20), "iota");
+    }
+
+    SECTION("views with adaptors")
+    {
+        auto first_3 = std::views::take(data, 3);
+        helpers::print(first_3, "first_3");
     }
 
     SECTION("pipes |")
     {
+        auto items = std::views::iota(1)
+            | std::views::take(20)
+            | std::views::filter([](int x)
+                { return x % 2 == 0; })
+            | std::views::transform([](int x)
+                { return x * x; })
+            | std::views::reverse;
+
+        for (const auto& item : items)
+        {
+            std::cout << std::format("item:{} ", item);
+        }
+        std::cout << "\n";
+    }
+
+    SECTION("keys-values")
+    {
+        std::map<int, std::string> dict = {{1, "one"}, {2, "two"}};
+
+        helpers::print(dict | std::views::keys, "keys");
+        helpers::print(dict | std::views::values, "values");
+        helpers::print(dict | std::views::elements<1>, "values");
     }
 }
 
 TEST_CASE("views - reference semantics")
-{    
-    std::vector data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+{
+    std::vector data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    auto evens_view = data | std::views::filter([](int i) { return i % 2 == 0; });
+    auto evens_view = data | std::views::filter([](int i)
+                          { return i % 2 == 0; });
     helpers::print(data, "data");
 
-    // TODO - set all even numbers to 0 using evens_view
-
+    std::ranges::fill(evens_view, 0);
     helpers::print(data, "data");
+}
+
+std::vector<std::string_view> tokenize(std::string_view text, auto separator = ' ')
+{
+    auto tokens = text | std::views::split(separator);
+
+    std::vector<std::string_view> tokens_sv;
+
+    for (auto&& rng : tokens)
+    {
+        // tokens_sv.push_back(std::string_view(&(*rng.begin()), rng.end() - rng.begin()));
+        tokens_sv.emplace_back(rng.begin(), rng.end());
+        // tokens_sv.emplace_back(rng); // C++23
+    }
+
+    return tokens_sv;
+}
+
+template <typename T>
+std::vector<std::span<T>> tokenize(std::span<T> text, auto separator)
+{
+    using Token = std::span<T>;
+
+    std::vector<Token> tokens;
+
+    for (auto&& rng : text | std::views::split(separator))
+    {
+        tokens.emplace_back(rng);
+    }
+
+    return tokens;
+}
+
+TEST_CASE("split")
+{
+    std::string str = "abc,def,ghi";
+
+    auto tokens = tokenize(str, ',');
+
+    helpers::print(tokens, "tokens");
+}
+
+template <typename TContainer>
+void custom_print(TContainer&& container)
+{
+    for (const auto& item : container)
+        std::cout << item << " ";
+    std::cout << "\n";
+}
+
+namespace AlternativeTake
+{
+    template <std::ranges::view T>
+    void custom_print(T container)
+    {
+        for (const auto& item : container)
+            std::cout << item << " ";
+        std::cout << "\n";
+    }
+}
+
+TEST_CASE("const fiasco")
+{
+
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7};
+    std::ranges::size(vec);
+    AlternativeTake::custom_print(std::views::all(vec));
+
+    custom_print(vec | std::views::take(3));
+
+    custom_print(vec | std::views::filter([](int x)
+                     { return x % 2 == 0; }));
 }
